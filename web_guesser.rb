ea3bf7bb533638a2ab2ce90419e5efe1f53094f7
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-11-15 17:08:31 -0700

    Refactors game.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,23 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'erb'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
 
 
 get '/' do
   guess = params["guess"]
-  message = check_guess(guess, number)
-  erb :index, :locals => {:number => number, :message => message}
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
 
-def check_guess(guess, number)
+def check_guess(guess)
   if !guess
     ""
-  elsif guess.to_i > number
+  elsif guess.to_i > SECRET_NUMBER
     "Too high"
-  elsif guess.to_i < number
+  elsif guess.to_i < SECRET_NUMBER
     "Too low"
-  elsif guess.to_i == number
-    "CORRECT! The SECRET NUMBER is #{number}"
+  else
+    "CORRECT! The SECRET NUMBER is #{SECRET_NUMBER}"
   end
 end
